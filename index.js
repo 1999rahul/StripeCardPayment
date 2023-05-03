02277ad5edcfa72334c8bde5036e2bafb909346a
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-05-03 21:46:55 +0530

    Charge saved cards

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const keys=require('./keys')
 const stripe = require('stripe')(keys.SecretKey)
 const express = require('express')
+const path=require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('./public'))
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/',(req,res)=>{
-    res.render("index.html")
+    res.send("index.html")
+})
+app.get('/admin',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public')+"/admin.html")
 })
 
 app.get('/setup-intents',async (req,res)=>{
@@ -22,6 +27,24 @@ app.post('/create_customer',async (req,res)=>{
         'payment_method':req.body.payment_method
       });
       res.send(JSON.stringify(customer))
+})
+
+app.post('/payment-intents',async (req,res)=>{
+    try{
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 2000,
+            currency: 'usd',
+            payment_method_types:['card'],
+            customer:req.body.customer,
+            payment_method:req.body.payment_method,
+            off_session:true,
+            conform:true
+          });
+          res.send(paymentIntent)
+    }
+    catch(e){
+        res.send(JSON.stringify(e))
+    }
 })
 
 app.listen(3000, ()=>{
